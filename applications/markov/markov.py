f901b12c9938eba4843abcb2c690aa16afab48bc
Drew Ross <mail.drew.ross@gmail.com>
2020-09-30 16:15:52 -0700

    Remove TODO.

--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -11,40 +11,38 @@ def markov_words(s):
     s = s.split(" ")
 
     for i, word in enumerate(s):
-        if word != '':
+        if word != "":
             if i + 1 < len(s):
                 if word not in words:
                     words[word] = []
-                if s[i + 1] != '':
+                if s[i + 1] != "":
                     words[word].append(s[i + 1])
 
     words = list(words.items())
     return words
 
-# TODO: construct 5 random sentences
-# Your code here
 
 def markov_sentence(d, rounds=1):
     if rounds < 1:
         rounds = 1
     for i in range(rounds):
-        output = ''
+        output = ""
 
         while True:
-            #choose a start word
+            # choose a start word
             word = random.choice(d)
             while word[0][-1] in '.!?"':
                 word = random.choice(d)
             word1 = word[0]
-            #choose a next word
+            # choose a next word
             word2 = random.choice(word[1])
-            output += f'{word1} {word2} '
-            #if there's punctuation at the end, break loop
-            if word2 != '' and word2[-1] in '.!?"':
+            output += f"{word1} {word2} "
+            # if there's punctuation at the end, break loop
+            if word2 != "" and word2[-1] in '.!?"':
                 break
 
         output = output.strip()
-        print(output, end='\n\n')
+        print(output, end="\n\n")
 
 
 mw = markov_words(words)
