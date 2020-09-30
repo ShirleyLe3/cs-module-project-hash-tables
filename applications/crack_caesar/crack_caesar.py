2f739352421c4c2605cbc6a5eca06677bc5b1cbe
Drew Ross <mail.drew.ross@gmail.com>
2020-09-30 13:55:15 -0700

    Complete crack_caesar. Test output.

--- applications/crack_caesar/crack_caesar.py
+++ applications/crack_caesar/crack_caesar.py
@@ -3,3 +3,65 @@
 
 # Your code here
 
+freq = {
+    "E": 11.53,
+    "T": 9.75,
+    "A": 8.46,
+    "O": 8.08,
+    "H": 7.71,
+    "N": 6.73,
+    "R": 6.29,
+    "I": 5.84,
+    "S": 5.56,
+    "D": 4.74,
+    "L": 3.92,
+    "W": 3.08,
+    "U": 2.59,
+    "G": 2.48,
+    "F": 2.42,
+    "B": 2.19,
+    "M": 2.18,
+    "Y": 2.02,
+    "C": 1.58,
+    "P": 1.08,
+    "K": 0.84,
+    "V": 0.59,
+    "Q": 0.17,
+    "J": 0.07,
+    "X": 0.07,
+    "Z": 0.03,
+}
+
+freq = list(freq.items())
+
+def crack(s):
+    count = dict()
+    cipher = "".join(s.split())
+    cipher = s.translate(str.maketrans("", "", ' \'\n":;,.-+=?!1/\\|[]{}()*^&â€”'))
+    
+    for l in cipher:
+        if l not in count:
+            count[l] = 1
+        count[l] += 1
+
+    count = list(count.items())
+    count.sort(key=lambda tup: tup[1], reverse=True)
+
+    decipher = dict()
+    for i, tup in enumerate(count):
+        decipher[tup[0]] = freq[i][0]
+
+    print(decipher)
+
+    output = ''
+    for l in s:
+        if l in decipher:
+            l = decipher[l]
+        output += l
+
+    print(output)
+
+
+f = open("ciphertext.txt", "r")
+crack(f.read())
+f.close()
--- applications/histo/histo.py
+++ applications/histo/histo.py
@@ -1,2 +1,29 @@
 # Your code here
+def histo(s):
+    # normalize whitespace, remove special characters
+    s = " ".join(s.split())
+    s = s.translate(str.maketrans("", "", '":;,.-+=/\\|[]{}()*^&')).lower().split(" ")
+    longest = 0
+    count = dict()
 
+    for word in s:
+        if word == "":
+            pass
+        elif word in count:
+            count[word] += 1
+        else:
+            count[word] = 1
+        if len(word) > longest:
+            longest = len(word)
+
+    count_list = list(count.items())
+    count_list.sort(key=lambda tup: (-tup[1], tup[0]))
+    
+    for tup in count_list:
+        spaces = ' ' * (longest - len(tup[0]))
+        hashes = '#' * tup[1]
+        print(f'{tup[0]}{spaces}{hashes}')
+
+f = open("robin.txt", "r")
+histo(f.read())
+f.close()
